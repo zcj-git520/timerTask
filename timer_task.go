178b2ab9c40522bcd9e8cbcef29f92a70140b2e5
zcj-git520 <71475834+zcj-git520@users.noreply.github.com>
2021-12-17 21:10:45 +0800

    add time task
    Signed-off-by: zcj-git520 <71475834+zcj-git520@users.noreply.github.com>

---
+++ timer_task.go
@@ -0,0 +1,171 @@
+package timerTask
+
+//定时单个或者对个任务：
+//1. 定时定时任务
+//2. 停止定时任务
+//3. 重置定时时间，并执行定时任务
+//4. 定时开启定时任务
+//5. 定时结束定时任务
+
+
+import (
+	log "github.com/sirupsen/logrus"
+	"sync"
+	"time"
+)
+
+// timer配置
+type timerConfig struct {
+	timing       time.Duration      // 定时时间
+	startTiming  time.Duration      // 定时开启定时任务时间
+	stopTiming   time.Duration      // 定时停止定时任务时间
+	running   	 bool               // 定时器运行的状态,运作中为true/没有运行为false
+	tasks        []func()           // 定时任务
+	stopChan     chan struct{}      // 停止定时器日任务
+	runningMu    sync.Mutex
+	Waiter       sync.WaitGroup
+}
+
+type Option func(*timerConfig)
+
+// 添加定时开启定时任务时间
+func SetTimerStart(d time.Duration) Option {
+	return func(t *timerConfig) {
+		t.startTiming = d
+	}
+}
+
+// 添加定时停止定时任务时间
+func SetTimerStop(d time.Duration) Option {
+	return func(t *timerConfig) {
+		t.stopTiming = d
+	}
+}
+
+// 添加定时时间
+func (t *timerConfig)addTiming(d time.Duration) {
+	t.timing = d
+}
+
+// 任务停止
+func (t *timerConfig)stop()  {
+	t.runningMu.Lock()
+	defer t.runningMu.Unlock()
+	if !t.running{
+		log.Info("The timer task is not started and cannot be stopped")
+		return
+	}
+	t.stopChan <- struct{}{}
+	t.running = false
+
+}
+
+// 任务执行
+func (t *timerConfig)run()  {
+	defer t.Waiter.Done()
+	t.running = true
+	for {
+		timer := time.NewTimer(t.timing)
+		select {
+		case <- timer.C:
+			for _, task := range t.tasks{
+				go task()
+			}
+		case <- t.stopChan:
+			timer.Stop()
+			log.Info("Timed task stop")
+			return
+		}
+	}
+}
+
+// 任务开始
+func (t *timerConfig)start()  {
+	t.runningMu.Lock()
+	defer t.runningMu.Unlock()
+	t.Waiter.Add(1)
+	defer t.Waiter.Done()
+	if t.running{
+		log.Info("The timer task has started")
+		return
+	}
+	go t.run()
+
+}
+
+func (t *timerConfig)Stop() {
+	t.stop()
+}
+
+func (t *timerConfig)Start() {
+	  t.start()
+}
+
+// 定时开启定时任务
+func (t *timerConfig)TimerStart() {
+	// 如果定时开启时间未开启或定时任务正在执行，直接返回
+	if t.startTiming <= 0 || t.running{
+		log.Info("The scheduled start time is not open or the scheduled task is being executed")
+		return
+	}
+	t.Waiter.Add(1)
+	go func() {
+		select {
+		case <- time.After(t.startTiming):
+			t.start()
+			t.Waiter.Done()
+			return
+		}
+	}()
+	t.Waiter.Wait()
+}
+
+// 定时停止定时任务
+func (t *timerConfig)TimerStop() {
+	// 如果定时结束时间未开启或定时任务未执行，直接返回
+	if t.stopTiming <= 0 || !t.running{
+		log.Info("The timing end time is not turned on or the timing task is not executed")
+		return
+	}
+	t.Waiter.Add(1)
+	go func() {
+		select {
+		case <- time.After(t.stopTiming):
+			t.stop()
+			t.Waiter.Done()
+			return
+		}
+	}()
+	t.Waiter.Wait()
+}
+
+// 重置定时时间
+func (t *timerConfig)Reset(d time.Duration)  {
+	if !t.running{
+		log.Info("The timer task is not started and cannot be stopped")
+		return
+	}
+	t.addTiming(d)
+	t.stop()
+	t.start()
+}
+
+// 得到运行状态
+func (t *timerConfig)GetRunStatus() bool {
+	return t.running
+}
+
+func NewTimerTask(d time.Duration, tasks []func(), opts ... Option) *timerConfig {
+	timer := &timerConfig{
+		timing:d,
+		startTiming:-1,
+		stopTiming:-1,
+		running:false,
+		tasks: tasks,
+		stopChan:make(chan struct{}),
+	}
+	for _, opt := range opts{
+		opt(timer)
+	}
+	return timer
+}
---
+++ timer_task_test.go
@@ -0,0 +1,223 @@
+package timerTask
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func printTest1()  {
+	fmt.Println("test1:", time.Now().Second())
+}
+
+func printTest2()  {
+	fmt.Println("test2:", time.Now().Second())
+}
+
+func printTest3()  {
+	fmt.Println("test3:", time.Now().Second())
+}
+
+func printTest4()  {
+	fmt.Println("test4:", time.Now().Second())
+}
+
+func printTest5()  {
+	fmt.Println("test5:", time.Now().Second())
+}
+
+// 测试开始和结束任务
+func TestNewTimerTask(t *testing.T) {
+	t1 := 1 * time.Second
+	tasks := []func(){printTest1, printTest2, printTest3, printTest4, printTest5}
+	timer := NewTimerTask(t1, tasks)
+	timer.Waiter.Add(2)
+	timer.start()
+	go func() {
+		select {
+		case <-time.After(5*time.Second):
+			timer.stop()
+			timer.Waiter.Done()
+			return
+		}
+
+	}()
+	timer.Waiter.Wait()
+	/*
+	=== RUN   TestNewTimerTask
+	test5: 10
+	test2: 10
+	test1: 10
+	test3: 10
+	test4: 10
+	test1: 11
+	test3: 11
+	test2: 11
+	test4: 11
+	test5: 11
+	test5: 12
+	test2: 12
+	test1: 12
+	test3: 12
+	test4: 12
+	test5: 13
+	test2: 13
+	test1: 13
+	test3: 13
+	test4: 13
+	time="2021-12-16T14:52:14+08:00" level=info msg="Timed task stop"
+	--- PASS: TestNewTimerTask (5.00s)
+	PASS
+	*/
+
+}
+
+// 测试重置定时时间任务
+func TestReset(t *testing.T) {
+	t1 := 1 * time.Second
+	tasks := []func(){printTest1, printTest2, printTest3, printTest4, printTest5}
+	timer := NewTimerTask(t1, tasks)
+	timer.Waiter.Add(3)
+	timer.start()
+	go func() {
+		select {
+		case <-time.After(10*time.Second):
+			timer.stop()
+			timer.Waiter.Done()
+			return
+		}
+
+	}()
+	go func() {
+		select {
+		case <-time.After(5*time.Second):
+			timer.Reset(2*time.Second)
+			timer.Waiter.Done()
+			return
+		}
+
+	}()
+	timer.Waiter.Wait()
+	/*
+	=== RUN   TestReset
+	test5: 55
+	test1: 55
+	test2: 55
+	test3: 55
+	test4: 55
+	test5: 56
+	test3: 56
+	test4: 56
+	test1: 56
+	test2: 56
+	test5: 57
+	test3: 57
+	test4: 57
+	test1: 57
+	test2: 57
+	test5: 58
+	test3: 58
+	test2: 58
+	test1: 58
+	test4: 58
+	time="2021-12-16T14:50:59+08:00" level=info msg="Timed task stop"
+	test5: 1
+	test3: 1
+	test2: 1
+	test4: 1
+	test1: 1
+	test5: 3
+	test1: 3
+	test2: 3
+	test3: 3
+	test4: 3
+	--- PASS: TestReset (10.00s)
+	PASS
+	*/
+}
+
+// 测试定时开始定时任务
+func TestTimerStart(t *testing.T) {
+	t1 := 1 * time.Second
+	ts := time.Now()
+	tasks := []func(){printTest1, printTest2, printTest3, printTest4, printTest5}
+	timer := NewTimerTask(t1, tasks, SetTimerStart(3*time.Second))
+	timer.TimerStart()
+	t.Logf("定时时间为：%v", time.Now().Sub(ts))
+	timer.Waiter.Add(2)
+	timer.start()
+	go func() {
+		select {
+		case <-time.After(5*time.Second):
+			timer.stop()
+			timer.Waiter.Done()
+			return
+		}
+
+	}()
+	timer.Waiter.Wait()
+	/*
+	=== RUN   TestTimerStart
+	    timer_task_test.go:146: 定时时间为：3.003182964s
+	time="2021-12-16T15:05:58+08:00" level=info msg="The timer task has started"
+	test2: 59
+	test1: 59
+	test3: 59
+	test4: 59
+	test5: 59
+	test5: 0
+	test3: 0
+	test4: 0
+	test1: 0
+	test2: 0
+	test5: 1
+	test1: 1
+	test2: 1
+	test3: 1
+	test4: 1
+	test5: 2
+	test1: 2
+	test2: 2
+	test3: 2
+	test4: 2
+	time="2021-12-16T15:06:03+08:00" level=info msg="Timed task stop"
+	--- PASS: TestTimerStart (8.01s)
+	PASS
+	*/
+}
+
+// 测试定时结束定时任务
+func TestTimerStop(t *testing.T) {
+	t1 := 1 * time.Second
+	tasks := []func(){printTest1, printTest2, printTest3, printTest4, printTest5}
+	timer := NewTimerTask(t1, tasks, SetTimerStop(3*time.Second))
+	timer.Waiter.Add(1)
+	timer.start()
+	time.Sleep(1*time.Second)
+	ts := time.Now()
+	timer.TimerStop()
+	t.Logf("定时时间为：%v", time.Now().Sub(ts))
+	timer.Waiter.Wait()
+	/*
+		=== RUN   TestTimerStop
+	test5: 8
+	test2: 8
+	test1: 8
+	test3: 8
+	test4: 8
+	test5: 9
+	test2: 9
+	test1: 9
+	test3: 9
+	test4: 9
+	test5: 10
+	test1: 10
+	test2: 10
+	test3: 10
+	test4: 10
+	time="2021-12-16T15:11:11+08:00" level=info msg="Timed task stop"
+	    timer_task_test.go:199: 定时时间为：3.001107379s
+	--- PASS: TestTimerStop (4.00s)
+	PASS
+	*/
+}
